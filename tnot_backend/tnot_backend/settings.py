0cac836fde86e0f1820e95f0a8cefe805f6cd676
Cameron <cam.macquarrie@gmail.com>
2024-11-21 10:16:10 -0500

    fixed nginx.conf route

--- tnot_backend/tnot_backend/settings.py
+++ tnot_backend/tnot_backend/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sdfdsfdsffdsfdsfds'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #bool(os.environ.get("DEBUG", default=0))
@@ -141,6 +141,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'tnot_backend.wsgi.application'
+ASGI_APPLICATION = 'tnot_backend.awsgi.application'
 
 
 # Database
