f989b927db16c8b6183f092e2cfd99b81a282304
Cameron <cam.macquarrie@gmail.com>
2024-11-25 15:08:04 -0500

    typo allowed host

--- tnot_backend/tnot_backend/settings.py
+++ tnot_backend/tnot_backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWS_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 SECURE_SSL_REDIRECT = False
 CSRF_COOKIE_SECURE = True
